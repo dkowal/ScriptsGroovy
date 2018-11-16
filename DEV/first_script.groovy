b797fc39ec5042fdaa5bc24e9fb1ce329b01e799
Dawid K <dawid.k.kowal@gmail.com>
2018-11-16 09:52:29 +0100

    Add files via upload
    added folders with script

---
+++ DEV/first_script.groovy
@@ -0,0 +1,4 @@
+String[] testArray = ["A", "B", "C", "D"]
+for(int i = 0; i < testArray.size(); i++) {
+	println(testArray[i])
+}
---
+++ TST/first_script.groovy
@@ -0,0 +1,4 @@
+String[] testArray = ["A", "B", "C"]
+for(int i = 0; i < testArray.size(); i++) {
+	println(testArray[i])
+}
